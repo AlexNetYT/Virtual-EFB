a61ec46719c0ba79be0eb2bfad8c38791f2b02ce
AlexNetYT <sascha@orlanm.ru>
2023-12-22 19:44:13 +0300

    Fixed downloading files

---
+++ static/ofp_script.js
@@ -0,0 +1,127 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var loadOFPBtn = document.getElementById("simbriefLoad");
+    var simbriefIdInput = document.getElementById("simbriefId");
+    var aircraftTypeSpan = document.getElementById("aircraftType");
+    var fl = document.getElementById("flightlevel");
+    var bt = document.getElementById("blocktime");
+    var fixesTableBody = document.querySelector("#fixTable tbody");
+    var loadTableBody = document.querySelector("#loadsheetTable tbody");
+
+    loadOFPBtn.addEventListener("click", function () {
+        // Mock API call to fetch OFP data based on SimBrief ID
+        var simbriefId = simbriefIdInput.value.trim();
+        if (simbriefId !== "") {
+            // Replace this with actual API endpoint for OFP data
+            fetch("/ofp/get_ofp_info/" + simbriefId)
+                .then(response => response.json())
+                .then(data => {
+                    // Update the page with OFP data
+                    updateOFPDetails(data);
+                    updateLoadsheet(data);
+                    
+                    loadOFPBtn.classList.add("animat-sbgd");
+                    setTimeout(function () {
+                        loadOFPBtn.classList.remove("animat-sbgd");
+                    }, 1500)
+                    setTimeout(function () {
+                        showPopups();
+                    }, 250)
+                    let map = data.map;
+                    let vertprof = data.vertprof;
+                    let pdf = data.pdf;
+                    bt.innerHTML = data.blockTime;
+                    fl.innerHTML = data.flightLevel;
+                    document.getElementById('map').src = map;
+                    document.getElementById('rtld').onclick = function() {changeIframe(map)};
+                    document.getElementById('vpld').onclick = function() {changeIframe(vertprof)};
+                    // document.getElementById('ofpld').onclick = function() {changeIframe(pdf)};
+                })
+                .catch(error => {
+                    loadOFPBtn.classList.add("animat-sbbd");
+                    setTimeout(function () {
+                        loadOFPBtn.classList.remove("animat-sbbd");
+                    }, 1500)
+                    console.error("Error fetching OFP information:", error);
+                });
+        }
+    });
+
+
+    function changeIframe(url) {
+        let view = document.getElementById('map');
+        view.src = url;
+    }
+    function showPopups(){
+        var animatedDiv = document.getElementById('animatedDiv');
+        var mapDiv = document.getElementById('map-container');
+        var ofpDiv = document.getElementById('ofp-container');
+
+        
+        // Toggle the 'visible-content' class
+        mapDiv.classList.toggle('visible-content');
+        animatedDiv.classList.toggle('visible-content');
+        // If the class is present, calculate and set the max-height
+        if (animatedDiv.classList.contains('visible-content')) {
+            animatedDiv.style.width = 'auto';
+            mapDiv.style.width = '35%';
+            ofpDiv.style.paddingRight = '5%';
+            setTimeout(function() {
+                animatedDiv.style.opacity = '1';
+                mapDiv.style.opacity = '1';
+            },50)
+            animatedDiv.style.maxHeight = animatedDiv.scrollHeight + 'px';
+            // mapDiv.style.maxHeight = mapDiv.scrollHeight + 'px';
+        } else {
+            animatedDiv.style.maxHeight = '0';
+            // mapDiv.style.maxHeight = '0';
+            setTimeout(function() {
+                animatedDiv.style.opacity = '0';
+                animatedDiv.style.width = '0px';
+                mapDiv.style.opacity = '0';
+                mapDiv.style.width = '0px';
+            },50)
+            
+        }
+    }
+
+
+    function updateOFPDetails(ofpData) {
+        var aircraftTypeSpan = document.getElementById("acType");
+        var fields = document.querySelector(".lblspan");
+        // var flightLevelSpan = document.getElementById("flightLevel");
+        // Update most important flight information
+        
+        // flightLevelSpan.textContent = ofpData.flightLevel;
+        var origin_f = document.getElementById("origin");
+        var dest_f = document.getElementById("destination");
+        aircraftTypeSpan.textContent = ofpData.callsign;
+        origin_f.textContent = ofpData.origin;
+        dest_f.textContent = ofpData.destination;
+        // Update the list of fixes
+        fixesTableBody.innerHTML = "";
+        ofpData.fixes.forEach(fix => {
+            var row = document.createElement("tr");
+            row.innerHTML = `<td>${fix.name}</td>
+                             <td>${fix.ident}</td>
+                             <td>${fix.via}</td>
+                             <td>${fix.fuelPlanOnboard}</td>
+                             <td>${fix.altitude}</td>`;
+            fixesTableBody.appendChild(row);
+        });
+        
+    }
+    function updateLoadsheet(ofpData) {
+        loadTableBody.innerHTML = "";
+            var row = document.createElement("tr");
+            row.innerHTML = `<td>${ofpData.loadsheet.block}</td>
+                             <td>${ofpData.loadsheet.enroute}</td>
+                             <td>${ofpData.loadsheet.pax}</td>
+                             <td>${ofpData.loadsheet.payload}</td>
+                             <td>${ofpData.loadsheet.zfw}</td>
+                             <td>${ofpData.loadsheet.tow}</td>`;
+            loadTableBody.appendChild(row);
+        
+        
+    }
+    
+});
---
+++ static/weather_script.js
@@ -0,0 +1,65 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var loadWeatherBtn = document.getElementById("loadWeatherBtn");
+    var icaoCodeInput = document.getElementById("icaoCode");
+    var weatherDetailsContainer = document.querySelector(".weather-details");
+
+    loadWeatherBtn.addEventListener("click", function () {
+        var icaoCode = icaoCodeInput.value.trim().toUpperCase();
+
+        // Check if the ICAO code is not empty
+        if (icaoCode.length === 4) {
+            // Make an asynchronous request to the Flask server to fetch weather info
+            fetch("/weather/get_weather_info/" + icaoCode)
+                .then(response => response.json())
+                .then(data => {
+                    // Update the weather details on the page
+                    updateWeatherDetails(data);
+                })
+                .catch(error => {
+                    console.error("Error fetching weather information:", error);
+                });
+        } else {
+            alert("Please enter a valid ICAO code.");
+        }
+    });
+
+    function updateWeatherDetails(weatherData) {
+        var weatherDetailsContainer = document.querySelector(".weather-details");
+        var weatherDetailsContainerId = document.getElementById("weather-details-container");
+        weatherDetailsContainer.innerHTML = "";
+        weatherDetailsContainer.classList.remove("opend");
+        weatherDetailsContainer.classList.add("closed");
+    
+        // Populate weather details on the page
+        for (const [key, value] of Object.entries(weatherData)) {
+            var p = document.createElement("p");
+            // var dib = document.createElement("div");
+            p.innerHTML = `<strong>${key}:</strong> ${value}`;
+            p.classList.add("weather_item");
+            // dib.innerHTML = `<div style="
+            //     width: 100%;
+            //     height: 5px;
+            //     background: #1f1f1f69;
+            //     border-radius: 189px;
+            // "></div>`;
+    
+            weatherDetailsContainer.appendChild(p);
+            // weatherDetailsContainer.appendChild(dib);
+    
+            // Trigger reflow to apply the initial state before transitioning
+            p.offsetHeight;
+            // dib.offsetHeight;
+    
+            // Apply class to trigger the transition
+            p.classList.add("show");
+            // dib.classList.add("show");
+        }
+    
+        // Trigger the transition after a short delay
+        setTimeout(function () {
+            weatherDetailsContainer.classList.remove("closed");
+            weatherDetailsContainer.classList.add("opend");
+        }, 1000);
+    }
+    
+});
